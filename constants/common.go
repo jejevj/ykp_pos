521b7603b701819a13e2924b0ad06804cf29d049
jejevj <wijaya.angga.j@gmail.com>
2025-03-28 20:30:47 +0700

    init

---
+++ constants/common.go
@@ -0,0 +1,13 @@
+package constants
+
+const (
+	ENUM_ROLE_ADMIN = "admin"
+	ENUM_ROLE_USER  = "user"
+	ENUM_ROLE_SU    = "super"
+
+	ENUM_RUN_PRODUCTION = "production"
+	ENUM_RUN_TESTING    = "testing"
+
+	ENUM_PAGINATION_LIMIT = 10
+	ENUM_PAGINATION_PAGE  = 1
+)
